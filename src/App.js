334b20e1c61e4f6520b11372d9afd36d4b155405
DatNguyen602 <ngoclanhlethi2@gmail.com>
2023-05-11 13:28:50 +0700

    m32

--- src/App.js
+++ src/App.js
@@ -3,18 +3,22 @@ import Home from './Components/Home';
 import HomeSignin from './Components/HomeSignin';
 import SignIn from './Components/SignIn';
 import { Routes , Route } from 'react-router';
+import store from "./app/store"
 
 function App() {
+  const {UserContext} = store;
+
   return (
-    <div className="App">
-      {/* <Home/> */}
-      <Routes>
-        <Route path='project-1/' element={<Home/>}></Route>
-        <Route path='project-1/signin' element={<SignIn/>}></Route>
-        <Route path='project-1/homesignin' element={<HomeSignin/>}></Route>
-        <Route path='*' element={<h1>404 Not Fount</h1>}></Route>
-      </Routes>
-    </div>
+    <UserContext.Provider value="">
+      <div className="App">
+        <Routes>
+          <Route path='project-1/' element={<Home/>}></Route>
+          <Route path='project-1/signin' element={<SignIn/>}></Route>
+          <Route path='project-1/homesignin' element={<HomeSignin/>}></Route>
+          <Route path='*' element={<h1>404 Not Fount</h1>}></Route>
+        </Routes>
+      </div>
+    </UserContext.Provider>
   );
 }
 
