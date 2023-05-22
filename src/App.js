f85e4ddfca9aa3d22d335121e171cc0d10c1af69
DatNguyen602 <ngoclanhlethi2@gmail.com>
2023-05-22 18:18:03 +0700

    tamplate commit

--- src/App.js
+++ src/App.js
@@ -1,7 +1,4 @@
 import './App.css';
-import Home from './Components/Home';
-import HomeSignin from './Components/HomeSignin';
-import SignIn from './Components/SignIn';
 import { Routes , Route } from 'react-router';
 import store from "./app/store"
 
@@ -12,10 +9,6 @@ function App() {
     <UserContext.Provider value={{}}>
       <div className="App">
         <Routes>
-          <Route path='project-1/' element={<Home/>}></Route>
-          <Route path='project-1/signin' element={<SignIn/>}></Route>
-          <Route path='project-1/homesignin' element={<HomeSignin/>}></Route>
-          <Route path='*' element={<h1>404 Not Fount</h1>}></Route>
         </Routes>
       </div>
     </UserContext.Provider>
